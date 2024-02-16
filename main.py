974430d9b7d91db5828bf21bd4fa519c8ac868bc
Aditya-Mane04 <140338667+Aditya-Mane04@users.noreply.github.com>
2024-02-17 00:42:34 +0530

    main.py

--- main.py
+++ main.py
@@ -7,12 +7,6 @@ import cv2
 import os
 import numpy as np
 
-
-# Parameters
-width, height = 1280, 720
-gestureThreshold = 300
-folderPath = "Presentation"
-
 # Camera Setup
 cap = cv2.VideoCapture(0)
 cap.set(3, width)
