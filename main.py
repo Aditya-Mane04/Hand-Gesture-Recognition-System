2e448366c1d327786c8a8775d7b42cd75502ea67
Aditya-Mane04 <140338667+Aditya-Mane04@users.noreply.github.com>
2024-02-17 00:15:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import messagebox
+import csv
+from datetime import datetime
+from cvzone.HandTrackingModule import HandDetector
+import cv2
+import os
+import numpy as np
+
+
+# Parameters
+width, height = 1280, 720
+gestureThreshold = 300
+folderPath = "Presentation"
+
+# Camera Setup
+cap = cv2.VideoCapture(0)
+cap.set(3, width)
+cap.set(4, height)
+
+# Hand Detector
+detectorHand = HandDetector(detectionCon=0.8, maxHands=1)
+
+# Variables
+imgList = []
+delay = 30
+buttonPressed = False
+counter = 0
+drawMode = False
+imgNumber = 0
+delayCounter = 0
+annotations = [[]]
+annotationNumber = -1
+annotationStart = False
+hs, ws = int(120 * 1), int(213 * 1)  # width and height of the small image
